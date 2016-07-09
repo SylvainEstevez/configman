ae9d0b36266557e51775c3464d6636e3c3fe13ce
Sylvain Estevez <estevez.sylvain@gmail.com>
2016-07-09 05:02:50 -0700

    :wrench: Travis and coveralls setup

--- GruntFile.js
+++ GruntFile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const coveralls = require('coveralls');
+
 module.exports = (grunt) => {
 
   grunt.loadNpmTasks('grunt-mocha-istanbul');
@@ -7,7 +9,7 @@ module.exports = (grunt) => {
   grunt.initConfig({
     mocha_istanbul: {
       coverage: {
-        src: 'test',
+        src: ['./test/**/*.test.js'],
         options: {
           scriptPath: require.resolve('./node_modules/.bin/istanbul'),
           mask: '*.test.js',
@@ -17,17 +19,32 @@ module.exports = (grunt) => {
         }
       },
       test: {
-        src: 'test',
+        src: ['./test/**/*.test.js'],
         options: {
           recursive: true,
           reporter: 'spec',
           require: ['test/common']
         }
+      },
+      coveralls: {
+        src: ['./test/**/*.test.js'],
+        options: {
+          coverage: true,
+          recursive: true,
+          quiet: true,
+          require: ['test/common'],
+          scriptPath: require.resolve('./node_modules/.bin/istanbul')
+        }
       }
     }
   });
 
+  grunt.event.on('coverage', function(lcov, done){
+    return coveralls.handleInput(lcov, done);
+  });
 
   grunt.registerTask('test', ['mocha_istanbul:test']);
+  grunt.registerTask('check', ['test']);
+  grunt.registerTask('coveralls', ['mocha_istanbul: coveralls']);
   grunt.registerTask('default', 'check');
 };
